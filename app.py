3cd15b64d7e7c6f949b7baf8a8c827eae6652ab1
Karolbarros <116102454+Karolbarros@users.noreply.github.com>
2024-03-05 13:33:48 +0000

    adicição das rotas gastos e dobro

--- app.py
+++ app.py
@@ -8,10 +8,19 @@ def index():
 
 @app.route('/compras')
 def compras():
-    return render_template('compras.html')
+    return render_template('compras.html',item1='farinha',item2='batata')
 
 @app.route('/mercados')
 def mercados():
     return render_template('mercados.html')
 
+@app.route('/gastos', defaults={'mes':'janeiro','valor':'0'})
+@app.route('/gastos/<mes>/<valor>')
+def gastos(mes, valor):
+    return render_template('gastos.html', mes = mes, valor = valor)
 
+@app.route('/dobro/<float:n>')
+@app.route('/dobro/<int:n>')
+def dobro(n):
+    resultado=2*n
+    return render_template("dobro.html",n=n,resultado=resultado)
